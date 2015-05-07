ecb74e96cad39bd5286972661fa6cc0e0f5bea79
HsuTing <hsuting0106@gmail.com>
2015-05-07 18:56:22 +0800

    follow half

--- hw2/Analyer.cpp
+++ hw2/Analyer.cpp
@@ -10,17 +10,22 @@ using namespace std;
 int count_index();
 void input_grammar(Data index[]);
 int first_check(Data index[], int max, string name);
-void find_first(Data index[], int max, string name, int mod);
+void find(Data index[], int max, string name);
+void find(Data index[], Data tempdata[], int max, string name, int position);
 void first(Data index[], int max);
-void find_follow(Data index[], int max, string name);
-void follow(Data index[], int max);
+void find_follow(Data index[], Data tempdata[], int max, string name, int position);
+void follow(Data index[], Data tempdata[], int max);
 
 int main(void) {
 	int max = count_index();
-	Data index[max];
+	Data index[max], tempdata[max];
 	input_grammar(index);
 	first(index, max);
-	follow(index, max);
+
+	for(int i = 0; i < max; i++) {
+		tempdata[i].set_name(index[i].get());
+		find_follow(index, tempdata, max, index[i].get(), i);
+	}
 
 	/*for(int i = 0; i < max; i++) {
 		cout << index[i].get() << ": ";
@@ -37,6 +42,14 @@ int main(void) {
 
 		cout << index[i].get_check_max() << endl;
 	}*/
+
+	for(int i = 0; i < max; i++) {
+		cout << tempdata[i].get() << ": ";
+		for(int j = 0; j < tempdata[i].get_max(); j++) {
+			cout << tempdata[i].at(j) << " ";
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
@@ -113,14 +126,10 @@ int first_check(Data index[], int max, string name) {
 	return -1;
 }
 
-void find_first(Data index[], int max, string name, int mod) {
+void find(Data index[], int max, string name) {
 	int temp = first_check(index, max, name), check = 0;
 
 	if(temp == -1) {
-		if(mod == 1 && name == "epsilon") {
-			return;
-		}
-
 		ofstream fp("set.txt", ios::app);
 		fp << name << " ";
 		fp.close();
@@ -130,7 +139,32 @@ void find_first(Data index[], int max, string name, int mod) {
 		for(int i = 0; i < index[temp].get_check_max(); i++) {
 			for(int j = 0; j < index[temp].get_max(); j++) {
 				if(index[temp].find(j) == i) {
-					find_first(index, max, index[temp].at(j), mod);
+					find(index, max, index[temp].at(j));
+					break;
+				}
+			}
+		}
+	}
+}
+
+void find(Data index[], Data tempdata[], int max, string name, int position) {
+	int temp = first_check(index, max, name), check = 0;
+
+	if(temp == -1) {
+		if(name == "epsilon") {
+			return;
+		}
+
+		temp = tempdata[position].get_max();
+		tempdata[position].set_subdata(temp, name);
+		tempdata[position].set_max(temp + 1);
+		return;
+	}
+	else {
+		for(int i = 0; i < index[temp].get_check_max(); i++) {
+			for(int j = 0; j < index[temp].get_max(); j++) {
+				if(index[temp].find(j) == i) {
+					find(index, tempdata, max, index[temp].at(j), position);
 					break;
 				}
 			}
@@ -152,7 +186,7 @@ void first(Data index[], int max) {
 		for(int j = 0; j < index[i].get_check_max(); j++) {
 			for(int k = 0; k < index[i].get_max(); k++) {
 				if(index[i].find(k) == j) {
-					find_first(index, max, index[i].at(k), 0);
+					find(index, max, index[i].at(k));
 					break;
 				}
 			}
@@ -164,42 +198,19 @@ void first(Data index[], int max) {
 	}
 }
 
-void find_follow(Data index[], int max, string name) {
+void find_follow(Data index[], Data tempdata[], int max, string name, int position) {
 	for(int i = 0; i < max; i++) {
 		for(int j = 0; j < index[i].get_max(); j++) {
 			if(index[i].at(j) == name) {
-				if(index[i].find(j + 1) != 0 && j + 1 < index[i].get_check_max()) {
+				if(index[i].find(j + 1) != index[i].find(j)) {
 				}
-				else if(index[i].get_max() - 1 == j) {
-				}
-				else {
-					for(int k = j; k < index[i].get_max(); k++) {
-						if(index[i].find(k) == index[i].find(j) + 1) {
-							find_first(index, max, index[i].at(k - 1), 1);
-						}
-					}
+				else if(index[i].get_max() != j + 1){
+					find(index, tempdata, max, index[i].at(j + 1), position);
 				}
 			}
 		}
 	}
 }
 
-void follow(Data index[], int max) {
-	ofstream fp("set.txt", ios::app);
-	fp << endl;
-	fp << "Follow" << endl;
-	fp.close();
-
-	for(int i = 0; i < max; i++) {
-		ofstream fp1("set.txt", ios::app);
-		fp1.setf(ios::left);
-		fp1 << setw(20) << index[i].get() << ": ";
-		fp1.close();
-
-		find_follow(index, max, index[i].get());
-
-		ofstream fp2("set.txt", ios::app);
-		fp2 << "$ " << endl;
-		fp2.close();
-	}
+void follow(Data index[], Data tempdata[], int max) {
 }
